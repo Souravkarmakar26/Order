5dd8f7850932bcf4bf7d422d2548a745947dc4e5
Sourav <sourav.karmakar@sap.com>
2022-09-12 14:53:22 +0530

    spring boot sleuth implementation

--- src/main/java/com/pos/order/controller/OrderController.java
+++ src/main/java/com/pos/order/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.pos.order.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,22 +20,24 @@ import com.pos.order.service.OrderService;
 
 @RestController
 public class OrderController {
+	
+	private static Logger log = LoggerFactory.getLogger(OrderController.class);
 
 	@Autowired
 	private OrderService orderService;
 	
 	@PostMapping(value = "/order", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> saveOrder(@RequestBody Order order){
-		System.out.println("---------"+order);
+		log.info("Payload - "+order);
 		orderService.saveOrder(order);
-		return new ResponseEntity<String>(HttpStatus.CREATED);
+		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 	
 	@PutMapping(value = "/order", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> updateOrder(@RequestBody Order order){
-		System.out.println("---------"+order);
+		log.info("Payload - "+order);
 		orderService.updateOrder(order);
-		return new ResponseEntity<String>(HttpStatus.ACCEPTED);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
 	}
 	
 	@GetMapping(value = "/orderList", produces = MediaType.APPLICATION_JSON_VALUE)
